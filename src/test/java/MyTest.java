d40aff3b1734d1e57dc16f8f7c16b1f3fd1a5fbf
Savotii <savotii@yandex.ru>
2017-12-26 15:03:57 +0200

    Попытка написания теста.

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -4,7 +4,10 @@ import static org.junit.Assert.*;
 
 import com.andersen.spring.controllers.ProductService;
 import com.andersen.spring.controllers.UserService;
+import com.andersen.spring.dao.DAO;
+import com.andersen.spring.dao.ProductDAO;
 import com.andersen.spring.entity.Product;
+import com.andersen.spring.impl.ProductDAOImpl;
 import com.andersen.spring.impl.ProductServiceImpl;
 import com.andersen.spring.impl.UserServiceImpl;
 import org.junit.Test;
@@ -24,15 +27,22 @@ public class MyTest {
     @Mock
     Product product;
 
+    @Mock
+    DAO productDAO;
+
     @Test
     public void TestServices() {
 
         UserService userServiceImpl = mock(UserServiceImpl.class);
         ProductService productServiceImpl = mock(ProductServiceImpl.class);
+
         Product product = mock(Product.class);
 
+        productDAO = mock(ProductDAOImpl.class);
+
         Product pr = new Product("title", "description", 10D, 1);
-        when(productServiceImpl.create(product)).thenReturn(product);
+        productDAO.created(pr);
+        when( productDAO.created(pr)).thenReturn(product);
 
         assertEquals(productServiceImpl.create(product), product);
 
