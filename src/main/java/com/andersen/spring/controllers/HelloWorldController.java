5dc3f496923e0c4438dcf27cf7ed094767fc960b
Savotii <savotii@yandex.ru>
2018-01-10 16:05:06 +0200

    Попытка реализации диспетчера

--- src/main/java/com/andersen/spring/controllers/HelloWorldController.java
+++ src/main/java/com/andersen/spring/controllers/HelloWorldController.java
@@ -1,4 +1,43 @@
 package com.andersen.spring.controllers;
 
-public class HelloWorldController {
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+@Controller
+public class HelloWorldController extends AbstractController {
+
+
+    @RequestMapping(value = "/")
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+                                                 HttpServletResponse response) throws Exception {
+
+        ModelAndView model = new ModelAndView("HelloWorldPage");
+        model.addObject("msg", "hello world");
+
+        return model;
+    }
+
+    @RequestMapping(value = "/start")
+    protected ModelAndView start()
+    {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("msg", "It's starting");
+        modelAndView.setViewName("HelloWorldPage");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/view")
+    protected ModelAndView view()
+    {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("msg", "It's viewing");
+        modelAndView.setViewName("HelloWorldPage");
+        return modelAndView;
+    }
 }
--- src/main/webapp/WEB-INF/pages/HelloWorldPage.jsp
+++ src/main/webapp/WEB-INF/pages/HelloWorldPage.jsp
@@ -1,8 +1,10 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page contentType = "text/html; charset = UTF-8" %>
 <html>
-<body>
-<h1>Spring MVC Hello World Example</h1>
+<head>
+    <title>Hello World</title>
+</head>
 
-<h2>${msg}</h2>
+<body>
+<h2>${message}</h2>
 </body>
 </html>
