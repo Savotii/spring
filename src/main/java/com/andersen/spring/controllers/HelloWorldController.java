01a5dbe882136e60cce04978fe88400bcb0eb566
Savotii <savotii@yandex.ru>
2018-01-11 16:10:15 +0200

    Попытка сделать свою 403

--- src/main/java/com/andersen/spring/controllers/HelloWorldController.java
+++ src/main/java/com/andersen/spring/controllers/HelloWorldController.java
@@ -16,11 +16,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.security.Principal;
 import java.util.Locale;
 
 @Controller
@@ -111,6 +113,21 @@ public class HelloWorldController {
         modelAndView.setViewName("errorPage");
         return "errorPage";
 
+    }
+
+    @RequestMapping(value = "/403")
+    public String accessDenied(Principal user, Model model)
+    {
+        if(user!=null)
+        {
+            model.addAttribute("msg", "" + user.getName() + ", sorry but you do not permission to access this page.");
+        }
+        else
+        {
+            model.addAttribute("msg", "You do not have permission to access this page");
+        }
+
+        return "403";
     }
 
 }
---
+++ src/main/webapp/WEB-INF/pages/403.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h1>HTTP Status 403 - Access is denied</h1>
+</div><h2>${msg}</h2>
+
+<p><a href="/index">На главную</a></p>
+</body>
+</html>
